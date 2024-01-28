2ad0f4c7bdaa9f6db11e779c735cb9edae5a9ef6
Alan Dorneles <alanricardo.97@gmail.com>
2024-01-28 16:13:12 -0300

    mudança nas rotas

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -10,7 +10,7 @@ export const Root = () => {
         <Routes>
             <Route path="/" element={<Container />} />
             <Route path="/radar" element={<Container />} />
-            <Route path="/estacao" element={<Container/>}></Route>
+            <Route path="/equipe" element={<Container/>}></Route>
             <Route path="/satelite" element={<Container/>}></Route>
         </Routes>
     </BrowserRouter>
