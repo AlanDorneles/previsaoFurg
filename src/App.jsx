14a9d09f29e8e0506889072eecd24f33d1be944b
Alan Dorneles <alanricardo.97@gmail.com>
2024-01-28 16:17:02 -0300

    retirada dos aninhamento de contextos

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,17 @@ import "./App.css";
 import "../node_modules/bulma/css/bulma.min.css";
 import "./sass/navbar.scss"
 import { Root } from "./routes/routes";
-import { HourScopeProvider } from "./contexts/hourAnimation";
-import { RadarIsCheckedProvider } from "./contexts/radarIsChecked";
-import { PreviousAndNextImageProvider } from "./contexts/previousAndNextImage";
-import { StationsVisibleProvider } from "./contexts/radarFilter";
-import { CodeStationsProvider } from "./contexts/codeStation";
-import { FilterTypeRadarProvider } from "./contexts/typeRadar";
+import { CombinedProviders } from "./contexts/_ContextProviders";
 
 function App() {
   return (
     <main className="container" style={{ maxWidth: "100vw" }}>
-      <HourScopeProvider>
-        <RadarIsCheckedProvider>
-          <PreviousAndNextImageProvider>
-            <StationsVisibleProvider>
-              <CodeStationsProvider>
-                <FilterTypeRadarProvider>
-                  <Root />
-                </FilterTypeRadarProvider>
-              </CodeStationsProvider>
-            </StationsVisibleProvider>
-          </PreviousAndNextImageProvider>
-        </RadarIsCheckedProvider>
-      </HourScopeProvider>
+      <CombinedProviders>
+          <Root />
+      </CombinedProviders>
+      
+                  
+              
     </main>
   );
 }
